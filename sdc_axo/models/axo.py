d308554295be396c46b23a0dd9ef9ea9cd10457e
aitmlouk <aitmlouk@addi>
2018-07-21 10:49:05 +0100

    Fix compute error

--- sdc_axo/models/axo.py
+++ sdc_axo/models/axo.py
@@ -68,12 +68,21 @@ class SaleOrder(models.Model):
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'   
 
-    @api.one
-    @api.depends('product_uom_qty','price_unit')
-    def _total_compute(self):
-        if self.product_uom_qty:
-            self.price_subtotal = 1+(self.product_uom_qty/100)*self.price_unit,
 
+    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
+    def _compute_amount(self):
+        """
+        Compute the amounts of the SO line.
+        """
+        for line in self:
+            price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+            taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
+            line.update({
+                'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
+                'price_total': taxes['total_included'],
+                'price_subtotal': self.price_unit*(1+(self.product_uom_qty/100)),
+            })
+            
     @api.onchange('product_id')
     def onchange_product(self):
         if self.product_id:
