c4abd1e7a9ba95ec11735cc213c6e0cbe015311d
aitmlouk <aitmlouk@addi>
2018-07-25 00:05:19 +0100

    C25072018 articles and quotation...

--- sdc_axo/models/axo.py
+++ sdc_axo/models/axo.py
@@ -26,7 +26,12 @@ class ResCompanyType(models.Model):
     
 class ProductTemplate(models.Model):
     _inherit = 'product.template' 
-    
+
+    @api.onchange('width','height')
+    def _compute_area(self):
+        if self.width and self.height:
+            self.area = self.width * self.height or False
+                
     internal_category_id = fields.Many2one('product.internal',string='Catégorie Emplac.')
     display_type_id = fields.Many2one('product.display',string='Type d\'affichage') 
     adress = fields.Char(string='Adresse') 
@@ -57,12 +62,24 @@ class ResCity(models.Model):
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order' 
-    
+       
+    @api.depends('order_line')
+    def _compute_display(self):
+            display = []
+            if self.order_line:
+                for l in self.order_line: 
+                    if l.product_id.display_type_id.name not in display:           
+                        display.append(l.product_id.display_type_id.name) 
+                display_name = ', '.join(str(v) for v in display)
+                self.display = display_name or False
+            return True
+                
+                
     compagne = fields.Char(string='Campagne')
     periode = fields.Char(string='Période demandée')
     annance = fields.Char(string='Annonceur')
+    display = fields.Char(compute='_compute_display',string='Choix d\'affichage')
     refrence_id = fields.Selection([('contract', 'Contrat de prestation'), ('print', 'Réimpression')], string= 'Référence')
-    display_id = fields.Many2one('product.display',string='Choix d\'affichage')
    
  
 class SaleOrderLine(models.Model):
@@ -80,18 +97,29 @@ class SaleOrderLine(models.Model):
             line.update({
                 'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
                 'price_total': taxes['total_included'],
-                'price_subtotal': self.price_unit*(1+(self.product_uom_qty/100)),
+                'price_subtotal': line.price_unit*(1+(line.product_uom_qty/100)),
             })
             
     @api.onchange('product_id')
     def onchange_product(self):
         if self.product_id:
             self.adresse = self.product_id.adress or False
-                                        
+            self.width = self.product_id.width or False
+            self.height = self.product_id.height or False
+            self.area = self.product_id.area or False
+
+    @api.onchange('width','height')
+    def _compute_area(self):
+        if self.width and self.height:
+            self.area = self.width * self.height or False
+                                                    
     adresse = fields.Char(string='Adresse')
     du = fields.Date(string='Du')
     au = fields.Date(string='Au')
     month_nbr = fields.Integer(string='Nbr mois') 
+    width = fields.Float(string='Largeur') 
+    height = fields.Float(string='Hauteur')
+    area = fields.Float(string='Surface')  
     dimension = fields.Char(string='Dimension')
     vailable = fields.Date(string='Disponibilité')
     product_uom_qty = fields.Float(string='Comm. Agence', digits=dp.get_precision('Product Unit of Measure'), required=True, default=0.0)
